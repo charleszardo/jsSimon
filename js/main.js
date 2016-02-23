ea113c5a6853cf579d2684111e38d57fa117d9b7
Dave Charnuska <davecharnuska@gmail.com>
2016-02-22 23:42:47 -0500

    cleanup

--- js/main.js
+++ js/main.js
@@ -20,58 +20,12 @@ $(document).ready(function() {
 		}
 	})
 	
-	function LightenDarkenColor(col, amt) {
-	    var usePound = false;
-  
-	    if (col[0] == "#") {
-	        col = col.slice(1);
-	        usePound = true;
-	    }
- 
-	    var num = parseInt(col,16);
- 
-	    var r = (num >> 16) + amt;
- 
-	    if (r > 255) r = 255;
-	    else if  (r < 0) r = 0;
- 
-	    var b = ((num >> 8) & 0x00FF) + amt;
- 
-	    if (b > 255) b = 255;
-	    else if  (b < 0) b = 0;
- 
-	    var g = (num & 0x0000FF) + amt;
- 
-	    if (g > 255) g = 255;
-	    else if (g < 0) g = 0;
- 
-	    return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
-	}
-
-	function hex(x) {
-	  return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
-	 }
-	 
-	//Function to convert rgb color to hex format
-	function rgb2hex(rgb) {
-	 rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-	 return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
-	}
-	
 	function nums () {
 		var keys = [];
 		for (var k in mapping) {
 			keys.push(Number(k));
 		}
 		return keys;
-	}
-	
-	function flash(el, val) {
-		(el).css("background-color", LightenDarkenColor(rgb2hex($(el).css("background-color")), val)); 
-	}
-	
-	function resetBg(el, val) {
-		$(el).css("background-color", val);
 	}
 	
 	function fade(el, val) {
