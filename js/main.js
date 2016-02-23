7b10f50d0a3433b488ddb1f6d1d0fbd6feb062b2
Dave Charnuska <davecharnuska@gmail.com>
2016-02-22 23:14:56 -0500

    add nums(), start click logic, globals, and expand game()

--- js/main.js
+++ js/main.js
@@ -1,14 +1,41 @@
 $(document).ready(function() {
 	var power = false,
-			strict = false,
-			count = 0;
+		  strict = false,
+			count = 0,
+			mapping = {
+				1: "green",
+				2: "red",
+				3: "blue",
+				4: "yellow"
+			};
 	
 	$("#power").click(function() {
 		power = !power;
-		alert(power)
 	})
 	
+	$("#start").click(function() {
+		if (power) {
+			game();
+		}
+	})
+	
+	function nums () {
+		var keys = [];
+		for (var k in mapping) {
+			keys.push(Number(k));
+		}
+		return keys;
+	}
+	
 	function game () {
-		
+		var play = true,
+		    options = nums(),
+			  sequence = [],
+			  curr_num;
+				
+		while (play) {
+			curr_num = options[Math.floor(Math.random()*options.length)];
+			play = false;
+		}
 	}
 })
