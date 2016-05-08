225bf26d8eb1df8e3d60b245cbedd8ab5b94b08a
Dave Charnuska <davecharnuska@gmail.com>
2016-05-08 11:11:21 -0400

    move game() to init() in game object

--- js/main.js
+++ js/main.js
@@ -5,12 +5,34 @@ $(document).ready(function() {
 		count: 0,
 		rounds: 0,
 		inPlay: false,
+		handler: false,
+		sequence: [],
+		playSequence: [],
     hexDigits: ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"],
 		mapping: {
 			1: "green",
 			2: "red",
 			3: "blue",
 			4: "yellow"
+		},
+		init: function () {
+			var options = nums(),
+				  sequence = [],
+				  curr_num;
+			
+			while (rounds < 5) {
+				// gen new num
+				curr_num = options[Math.floor(Math.random()*options.length)];
+				// add to seq
+				sequence.push(curr_num);
+				// play seq
+				console.log(sequence);
+				// user input
+				inPlay = true;
+				playRound(sequence);
+				inPlay = false;
+				rounds++;
+			}
 		}
 	}
 	
@@ -54,24 +76,4 @@ $(document).ready(function() {
 			
 		}
 	})
-	
-	function game () {
-		var options = nums(),
-			  sequence = [],
-			  curr_num;
-				
-		while (rounds < 5) {
-			// gen new num
-			curr_num = options[Math.floor(Math.random()*options.length)];
-			// add to seq
-			sequence.push(curr_num);
-			// play seq
-			console.log(sequence);
-			// user input
-			inPlay = true;
-			playRound(sequence);
-			inPlay = false;
-			rounds++;
-		}
-	}
 })
