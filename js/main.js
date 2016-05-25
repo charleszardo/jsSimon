64d1042994fec0477cb42671a561270f5e28a5a8
Dave Charnuska <davecharnuska@gmail.com>
2016-05-25 16:29:53 -0400

    clean up and temporarily organize vars/functions by whats being used

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
 var game = {
 	power: true,
-	strict: false,
-	count: 0,
-	rounds: 0,
-	turn: 0,
-	level: 0,
 	opacityHigh: '1',
 	opacityLow: '0.6',
 	playerTurn: false,
 	handler: false,
 	inGame: false,
-	handlers: false,
 	sequence: [],
 	currSequence: [],
+	
+	
+	
+	
+	strict: false,
+	count: 0,
+	rounds: 0,
+	turn: 0,
+	level: 0,
+	
 	shape: "#shape",
   hexDigits: ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"],
 	mapping: {
@@ -21,6 +25,7 @@ var game = {
 		3: "blue",
 		4: "yellow"
 	},
+	
 	flash: function(el, times, speed, pad) {
 		var that = this;
 		
@@ -41,18 +46,6 @@ var game = {
 			times -= 1;
 		}
 	},
-	nums: function () {
-		var keys = [];
-		for (var k in this.mapping) {
-			keys.push(Number(k));
-		}
-		return keys;
-	},
-	playSound: function(clip) {
-		var sound = $(".sound"+clip)[0];
-		sound.currentTime = 0;
-		sound.play();
-	},
 	addToSequence: function () {
 		var options = this.nums(),
 		    nextNum = options[Math.floor(Math.random()*options.length)];
@@ -67,6 +60,48 @@ var game = {
 				that.flash($(that.shape+val),1,300,val)
 			}, 500 * idx)
 		});
+	},
+	init: function () {
+		if (!this.handler) {
+			this.initPadHandler();
+		}
+		
+		this.newGame();
+	},
+	newGame: function () {
+	  this.computerTurn();
+	},
+	computerTurn: function () {
+		this.addToSequence();
+		this.playSequence();
+		this.playerTurn = true;
+	},
+	initPadHandler: function () {
+		var that = this;
+		
+		$(".pad").click(function () {
+			console.log(that.playerTurn);
+			if (that.playerTurn) {
+				console.log(this);
+			}
+		})
+		
+		this.handler = true;
+	},
+	
+	
+	
+	nums: function () {
+		var keys = [];
+		for (var k in this.mapping) {
+			keys.push(Number(k));
+		}
+		return keys;
+	},
+	playSound: function(clip) {
+		var sound = $(".sound"+clip)[0];
+		sound.currentTime = 0;
+		sound.play();
 	},
 	playerMove: function(pad) {
 		this.currSequence.push(pad);
@@ -104,42 +139,8 @@ var game = {
 	},
 	updateScore: function() {
 		
-	},
-	init: function () {
-		if (!this.handler) {
-			this.initPadHandler();
-		}
-		
-		this.newGame();
-	},
-	newGame: function () {
-	  this.computerTurn();
-	},
-	computerTurn: function () {
-		this.addToSequence();
-		this.playSequence();
-		this.playerTurn = true;
-	},
-	initPadHandler: function () {
-		var that = this;
-		console.log('initialized');
-		
-		$(".pad").click(function () {
-			console.log(that.playerTurn);
-			if (that.playerTurn) {
-				console.log(this);
-			}
-		})
-		
-		this.handlers = true;
 	}
 }
-
-// $(".pad").click(function() {
-// 	if (true) {
-//
-// 	}
-// })
 
 $(document).ready(function() {
 	$(".power").click(function() {
