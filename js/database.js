7518100cf61622ad34ebf6b72e4ba5d00230a0d8
Dave Charnuska <davecharnuska@gmail.com>
2016-05-27 00:42:48 -0400

    clean up

--- js/database.js
+++ js/database.js
@@ -1,24 +1,14 @@
 // Initialize Firebase
-var config = {
+var config,
+    database;
+
+config = {
   apiKey: "AIzaSyCMPggR7F8NTqpCnDO3vJU0xMKc6fBUb-0",
   authDomain: "simon-f41bd.firebaseapp.com",
   databaseURL: "https://simon-f41bd.firebaseio.com",
   storageBucket: "",
 };
+
 firebase.initializeApp(config);
 
-var database = firebase.database();
-database.ref('high-score').on('value', function(snapshot) {
-  console.log(snapshot.val());
-});
-database.ref('high-score').set({
-	score: 0
-});
-console.log('123');
-database.ref('high-score').set({
-	score: 1
-});
-console.log('xyz');
-database.ref('high-score').once('value').then(function(snapshot){
-	console.log(snapshot.val());
-})
+database = firebase.database();
